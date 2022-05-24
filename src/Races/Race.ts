afb19950c2af6e0139ec4bce3fb2ba55d0cbae3f
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-05-24 18:10:27 -0300

    feat: importar as e exportar as classes Dwarf, Elf, Halfling e Orc

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,12 +1,10 @@
 abstract class Race {
   private _name: string;
   private _dexterity: number;
-  // protected _consructorCounter = 0;
 
   constructor(name: string, dexterity: number) {
     this._name = name;
     this._dexterity = dexterity;
-    // this._consructorCounter += 1;
   }
 
   get name(): string {
