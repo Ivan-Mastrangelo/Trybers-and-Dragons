d971b3e264f4563392037d4a424447a1d28c6fad
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-05-25 16:20:06 -0300

    refactor: importar a interface SimpleFighter e alterar o type de enemy no método attack para SimpleFighter

--- src/Character.ts
+++ src/Character.ts
@@ -1,4 +1,4 @@
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
 import Race, { Elf } from './Races';
@@ -62,7 +62,7 @@ class Character implements Fighter {
     return this._lifePoints;
   }
 
-  attack(enemy: Fighter): void {
+  attack(enemy: SimpleFighter): void {
     enemy.receiveDamage(this._strength);
   }
 
